c3824df8f565f0518435b8d3f7bddd4b785e5e95
Julmuendi <Julmuia@gmail.com>
2022-01-21 12:09:00 +0300

    Formatted the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
+function formatDate(timestamp){
+    let date=new Date(timestamp);
+    let days=["Sunday","Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let day=days[date.getDay()];
+    let hours=date.getHours();
+    if(hours<10){
+        hours=`0${hours}`;
+    }
+    let minutes=date.getMinutes();
+    if (minutes<10){
+        minutes=`0${minutes}`;
+    }
+    return `${day} ${hours}:${minutes}`;
+}
 function displayWeather(response){
     let temperatureElement=document.querySelector("#temp");
     temperatureElement.innerHTML=Math.round(response.data.main.temp);
@@ -10,6 +24,8 @@ function displayWeather(response){
     let skyDescriptionElement=document.querySelector("#skydescription");
     skyDescriptionElement.innerHTML=response.data.weather[0].description;
     cityElement.innerHTML=response.data.name;
+    let dateElement=document.querySelector("#date");
+    dateElement.innerHTML=formatDate(response.data.dt*1000);
 }
 
 let apiKey="ce735fca9b371504301605240e8fbfe8";
